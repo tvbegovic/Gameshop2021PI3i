0236c08112c74c390cff1be4cbbf258bf5e1223a
Tvrtko Begović <tvrtko@adamant.hr>
2022-01-30 10:08:01 +0100

    Riješena vježba 2. dio gradiva

---
+++ OnlineShopWeb/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineShopWeb.Db;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace OnlineShopWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private MyDbContext context;
+        public ProductController(MyDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("")]
+        public List<Product> GetProducts() 
+        {
+            return context.Products.Include(p => p.Category).Include(p => p.Manufacturer).ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProduct(int id)
+        {
+            return context.Products.FirstOrDefault(p => p.ID == id);
+        }
+
+        [HttpPost("")]
+        [Authorize]
+        public Product Create(Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+            return product;
+        }
+
+        [HttpPut("")]
+        [Authorize]
+        public Product Update(Product product)
+        {
+            context.Products.Update(product);
+            context.SaveChanges();
+            return product;
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public void Delete(int id)
+        {
+            context.Database.ExecuteSqlInterpolated($"DELETE FROM Product WHERE id = {id}");
+        }
+
+        [HttpGet("search")]
+        public List<Product> Search(string name, decimal priceFrom, decimal priceTo)
+        {
+            return context.Products.Where( p => p.Name.Contains(name) || (p.Price >= priceFrom && p.Price <= priceTo)).ToList();
+        }
+    }
+}
---
+++ OnlineShopWeb/Startup.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using OnlineShopWeb.Db;
+using OnlineShopWeb.JWT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MyDbContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("connString")
+                ));
+            //Token konfiguracija
+            var jwtTokenConfig = Configuration.GetSection("jwtTokenConfig").Get<JwtTokenConfig>();
+            services.AddSingleton(jwtTokenConfig);
+            services.AddScoped<IJwtAuthManager, JwtAuthManager>();
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtTokenConfig.Issuer,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtTokenConfig.Secret)),
+                    ValidAudience = jwtTokenConfig.Audience,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromMinutes(1)
+                };
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
